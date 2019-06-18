1d133de0e6daaf9450e7409e85d229ee2e3b37cc
lijun <lijun@git.com>
2019-06-18 21:59:46 +0800

    'shellsort'

--- src/com/sort/ShellSort.java
+++ src/com/sort/ShellSort.java
@@ -1,20 +1,59 @@
 package com.sort;
 
+import java.util.Arrays;
+
 /**
- * 实现希尔排序
+ * 采用交换的方式实现希尔排序：是一种优化后的插入排序
+ *
  */
 public class ShellSort {
     public static void main(String[] args) {
         int[] num = new int[]{4,1,2,-1,5,9,0,7,12,3};
-        num = ShellSort.sort(num);
+        num = ShellSort.sortByMove(num);
 
         for(int i : num){
             System.out.print(i + " ");
         }
     }
+    /**采用交换的形式来排序*/
+    public static int[] sortByExchange(int[] num){
+        int count = 0;
+        int times = num.length / 2;
+        while(0 != times){
+            for(int i = times;i < num.length;i++){
+                for(int j = i - times;j >= 0;j -=times){
+                    if(num[j] > num[j + times]) {
+                        int temp = 0;
+                        temp = num[j];
+                        num[j] = num[j + times];
+                        num[j + times] = temp;
+                    }
+                }
+            }
+            System.out.println("第 " + (++count) + " 轮排序 " + Arrays.toString(num));
+            times /= 2;
+        }
+        return num;
+    }
 
-    public static int[] sort(int[] num){
-
+    /**采用移动的方式来排序*/
+    public static int[] sortByMove(int[] num){
+        int count = 0;
+        int times = num.length / 2;
+        while(0 != times){
+            for (int i = times;i < num.length;i++){
+                int temp = num[i];
+                int j = i - times;
+                while(j  >= 0 && temp < num[j]){
+                    /**移动*/
+                    num[j + times] = num[j];
+                    j -= times;
+                }
+                num[j + times] = temp;
+                System.out.println("第 " + (++count) + " 轮排序 " + Arrays.toString(num));
+            }
+            times /= 2;
+        }
         return num;
     }
 }
