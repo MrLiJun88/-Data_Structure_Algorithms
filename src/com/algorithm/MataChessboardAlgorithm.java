9d5214586617dd61d985b2ce553d75668b02e274
lijun <lijun@git.com>
2019-07-24 11:45:01 +0800

    'over'

--- src/com/algorithm/MataChessboardAlgorithm.java
+++ src/com/algorithm/MataChessboardAlgorithm.java
@@ -3,9 +3,15 @@ package com.algorithm;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
- * 马踏棋盘算法
+ * 马踏棋盘算法(经过了贪心算法的优化，sort(list)方法)
+ * 马踏棋盘问题(骑士周游问题)实际上是图的深度优先搜索(DFS)的应用。
+ * 如果使用回溯（就是深度优先搜索）来解决，
+ * 假如马儿踏了53个点，如图：走到了第53个，坐标（1,0）
+ * 发现已经走到尽头，没办法，那就只能回退了，
+ * 查看其他的路径，就在棋盘上不停的回溯……
  */
 public class MataChessboardAlgorithm {
     /**棋盘的列数*/
@@ -18,6 +24,7 @@ public class MataChessboardAlgorithm {
     static boolean isFinished;
 
     public static void main(String[] args) {
+        /**X,Y 设置棋盘行列为8*8大小的棋盘*/
         X = 8;
         Y = 8;
         int row = 1;
@@ -29,7 +36,7 @@ public class MataChessboardAlgorithm {
         long start = System.currentTimeMillis();
         MataChessboardAlgorithm.startWalking(checkerboard,row - 1,col - 1,step);
         long end = System.currentTimeMillis();
-        System.out.println("运行时间: " + String.valueOf(end - start) + " sm");
+        System.out.println("运行时间: " + String.valueOf(end - start) + " ms");
         /**输出棋盘的最后情况*/
         for(int[] rows :checkerboard){
             System.out.println(Arrays.toString(rows));
@@ -44,12 +51,13 @@ public class MataChessboardAlgorithm {
      * @param step 是第几步，初始是从1开始，后面递增
      */
     public static void startWalking(int[][] checkerboard,int row,int col,int step){
-        System.out.println("游戏开始...");
         checkerboard[row][col] = step;
         /**标记该位置被访问过了*/
         isVisited[row * X + col] = true;
         /**获取当前位置可以走的下一个位置的集合*/
         ArrayList<Point> list = MataChessboardAlgorithm.next(new Point(row,col));
+        /**对list进行排序，对list中所有的Point数目进行非递减排序*/
+        MataChessboardAlgorithm.sort(list);
         /**遍历集合中的所有位置，走得通就走，走不通就回溯*/
         while(! list.isEmpty()){
             Point nextPoint = list.remove(0);
@@ -90,7 +98,7 @@ public class MataChessboardAlgorithm {
         if(condition2){
             list.add(new Point(point1));
         }
-        boolean condition3 = ((point1.x = curPoint.x + 1) < 0 && (point1.y = curPoint.y - 2) >= 0);
+        boolean condition3 = ((point1.x = curPoint.x + 1) < X && (point1.y = curPoint.y - 2) >= 0);
         if(condition3){
             list.add(new Point(point1));
         }
@@ -116,4 +124,24 @@ public class MataChessboardAlgorithm {
         }
         return list;
     }
+    /**根据当前这一步的下一步可走的集合进行非递减排序，减少回溯次数*/
+    public static void sort(ArrayList<Point> list){
+        list.sort(new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                int count1 = MataChessboardAlgorithm.next(o1).size();
+                int count2 = MataChessboardAlgorithm.next(o2).size();
+                if(count1 < count2){
+                    return -1;
+                }
+                else if(count1 == count2){
+                    return 0;
+                }
+                else {
+                    return 1;
+                }
+            }
+        });
+    }
+
 }
